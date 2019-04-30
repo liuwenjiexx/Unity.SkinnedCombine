4ac8068f419f752a9a99650ea33535a4cf5b4ac2
liuwenjie <liuwenjiexx@sina.com>
2019-04-30 15:02:25 +0800

    1

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Extensions
+{
+
+    public static Transform FindByName(this Transform t, string name)
+    {
+        Transform result = null;
+        foreach (Transform child in t)
+        {
+            if (child.name == name)
+            {
+                result = child;
+                break;
+            }
+            result = FindByName(child, name);
+            if (result != null)
+                break;
+        }
+
+        return result;
+    }
+}
